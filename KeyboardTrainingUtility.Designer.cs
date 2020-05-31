cda6ef76ed54a0c7cab30370376847b0f6052b71
Borna Vincek <borna.vincek1@gmail.com>
2020-05-31 22:50:52 +0200

    Initial commit

---
+++ KeyboardTrainingUtility.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace KeyboardTrainingUtility
+{
+    partial class KTUMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxMain = new System.Windows.Forms.RichTextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxMain
+            // 
+            this.textBoxMain.DetectUrls = false;
+            this.textBoxMain.Location = new System.Drawing.Point(11, 26);
+            this.textBoxMain.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxMain.Name = "textBoxMain";
+            this.textBoxMain.ReadOnly = true;
+            this.textBoxMain.Size = new System.Drawing.Size(800, 200);
+            this.textBoxMain.TabIndex = 0;
+            this.textBoxMain.Text = "";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(30, 378);
+            this.startButton.Margin = new System.Windows.Forms.Padding(2);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(94, 38);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Add Text";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // KTUMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 426);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.textBoxMain);
+            this.Name = "KTUMain";
+            this.Text = "Keyboard Training Utility";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.main_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.RichTextBox textBoxMain;
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ KeyboardTrainingUtility.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyboardTrainingUtility
+{
+    public partial class KTUMain : Form
+    {
+        public string tempStoredText = Clipboard.GetText();
+        public string tempSentanceInput;
+        public string tempPressedKey;
+        public int courrentPosition;
+
+
+        public KTUMain()
+        {
+            InitializeComponent();
+            textBoxMain.Text = tempStoredText;
+        }
+
+
+        private void main_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            editTextColor(e);
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        void setTextBoxText()
+        {
+
+        }
+        void editTextColor(KeyPressEventArgs e)
+        {
+            if (textBoxMain.TextLength != courrentPosition)
+            {
+                courrentPosition = textBoxMain.TextLength;
+            }
+
+
+            normalUnderline(2, "Black");
+            //textBoxMain.SelectionStart = courrentPosition;
+            //textBoxMain.SelectionLength = 2;
+            //textBoxMain.SelectionColor = Color.Black;
+            //textBoxMain.SelectionFont = new Font("Microsoft Sans Serif", 13, FontStyle.Underline);
+
+
+            if (e.KeyChar == (char)Keys.Back) //pressing Backspace  //add esc key
+            {
+
+                if (courrentPosition > 0)
+                {
+                    textBoxMain.SelectionStart = courrentPosition;
+                    textBoxMain.SelectionLength = textBoxMain.TextLength - courrentPosition;
+                    textBoxMain.SelectionColor = Color.Black;
+                    textBoxMain.SelectionFont = new Font("Microsoft Sans Serif", 13, FontStyle.Regular); //make everithing in front of the selected Black
+
+                    normalUnderline(1, "Black");//make sure that the underline is persistant
+
+                    courrentPosition--;
+                }
+                else
+                {
+                    textBoxMain.SelectionStart = courrentPosition;
+                    textBoxMain.SelectionLength = textBoxMain.TextLength - courrentPosition;
+                    textBoxMain.SelectionColor = Color.Black;
+                    textBoxMain.SelectionFont = new Font("Microsoft Sans Serif", 13, FontStyle.Regular);
+                    return;
+                }
+                return;
+            }
+            if (courrentPosition < textBoxMain.TextLength)
+            {
+                if (tempStoredText[courrentPosition] == e.KeyChar) //key press logic
+                {
+                    textBoxMain.SelectionStart = courrentPosition;
+                    textBoxMain.SelectionLength = 1;
+                    textBoxMain.SelectionColor = Color.Green;
+                    textBoxMain.SelectionFont = new Font("Microsoft Sans Serif", 13, FontStyle.Regular);
+                    courrentPosition++;
+                }
+                else
+                {
+                    textBoxMain.SelectionStart = courrentPosition;
+                    textBoxMain.SelectionLength = 1;
+                    textBoxMain.SelectionColor = Color.Red;
+                    textBoxMain.SelectionFont = new Font("Microsoft Sans Serif", 13, FontStyle.Regular);
+                    courrentPosition++;
+
+                }
+
+            }
+
+        }
+        void normalUnderline() //underlines the courrent selected letter, length is the amount of letters selected, color is what color the selected pice is;
+        {
+            textBoxMain.SelectionStart = courrentPosition;
+            textBoxMain.SelectionLength = 1;
+            textBoxMain.SelectionColor = Color.Black;
+            textBoxMain.SelectionFont = new Font("Microsoft Sans Serif", 13, FontStyle.Underline);
+        }
+        void normalUnderline(int length, string color) //underlines the courrent selected letter, length is the amount of letters selected, color is what color the selected pice is;
+        {
+            textBoxMain.SelectionStart = courrentPosition;
+            textBoxMain.SelectionLength = length;
+            color.ToUpper();
+            switch (color)
+            {
+                case "Red":
+                    textBoxMain.SelectionColor = Color.Red;
+                    break;
+
+                case "Green":
+                    textBoxMain.SelectionColor = Color.Green;
+                    break;
+
+                default:
+                    textBoxMain.SelectionColor = Color.Black;
+                    break;
+            }
+            textBoxMain.SelectionFont = new Font("Microsoft Sans Serif", 13, FontStyle.Underline);
+        }
+        void totalReset()
+        {
+            
+        }
+
+        
+    }
+}
